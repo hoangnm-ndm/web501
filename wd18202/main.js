852781389bc49c45c31d05419b77b8cd8f0ec5e0
hoangnm.ndm <hoangnm.ndm@gmail.com>
2023-09-22 15:35:17 +0700

    delete comment

--- wd18202/main.js
+++ wd18202/main.js
@@ -5,32 +5,13 @@ import AboutPage from "./src/pages/AboutPage";
 import Products from "./src/pages/Products";
 import AdminPage from "./src/pages/admin/AdminPage";
 
-// document.querySelector("#app").innerHTML = `
-//   <div>
-//    Xin chao cac ban!
-//   </div>
-// `;
-
-/**
- * Home Page, About Page, Contact Page, Admin Page...
- *
- */
-// const Welcome = () => {
-//   return `<div>Xin chao cac ban!</div>`;
-// };
-
-/**
- * http://127.0.0.1:5137/about -> about page
- * "/contact" -> contact page
- */
-
 const router = new Navigo("/", { linksSelector: "a" });
 const app = document.getElementById("app");
 
 router.on("/home", () => render(app, HomePage));
 router.on("/about", () => render(app, AboutPage));
-router.on("/products", () => Products());
-router.on("/admin", () => AdminPage());
+router.on("/products", () => render(app, Products));
+router.on("/admin", () => render(app, AdminPage));
 router.resolve();
 
 function render(target, content) {

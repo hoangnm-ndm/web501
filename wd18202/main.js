1086cee1c78be907226a0cc57430d2ba24b770b0
hoangnm.ndm <hoangnm.ndm@gmail.com>
2023-09-27 18:01:02 +0700

    Day 8: json-server, async await

--- wd18202/main.js
+++ wd18202/main.js
@@ -2,7 +2,7 @@ import "./style.css";
 import Navigo from "navigo";
 import HomePage from "./src/pages/HomePage";
 import AboutPage from "./src/pages/AboutPage";
-import Products from "./src/pages/Products";
+import Products, { GetProductsList } from "./src/pages/Products";
 import AdminPage from "./src/pages/admin/AdminPage";
 
 const router = new Navigo("/", { linksSelector: "a" });
@@ -10,7 +10,7 @@ const app = document.getElementById("app");
 
 router.on("/home", () => render(app, HomePage));
 router.on("/about", () => render(app, AboutPage));
-router.on("/products", () => render(app, Products));
+router.on("/products", () => render(app, GetProductsList));
 router.on("/admin", () => render(app, AdminPage));
 router.resolve();
 
--- wd18202/src/pages/Products.js
+++ wd18202/src/pages/Products.js
@@ -1,7 +1,16 @@
 import { products } from "../data_fake/datas";
 
+export async function GetProductsList() {
+  const response = await fetch("http://localhost:3000/products", {
+    method: "GET",
+  });
+  const products = await response.json();
+  console.log(products);
+}
+
 const ProductsList = () => `
 <div>
+
   ${products
     .map((product) => `<li class="product-item">${product.name}</li>`)
     .join(" ")}
